66b3e8e787d7105f1a47aae6c61ee0b14fb5cf8a
pascal22p <pascal22p@parois.net>
2018-10-03 20:12:17 +0000

    option to pass file as argument
    git-svn-id: https://svn.debroglie.net/crystals/trunk@7126 4ce1adb0-6e93-48c6-b531-a91e7d66e1f5

--- crystals/make_pretty.sh
+++ crystals/make_pretty.sh
@@ -2,7 +2,15 @@
 
 command -v fprettify >/dev/null 2>&1 || { echo >&2 "I require fprettify but it's not installed.  Aborting."; exit 1; }
 
-for i in $( ls *.F90 ); do
-  echo fprettify: $i
-  fprettify -i 2 -w 1 $i
-done
+if [ $# -eq 0 ] ; then
+  for i in $( ls *.F90 ); do
+    echo fprettify: $i
+    fprettify -i 2 -w 1 $i
+  done
+else
+  for file in "$@"; do
+    echo fprettify: $file
+    fprettify -i 2 -w 1 $file
+  done
+fi
+
