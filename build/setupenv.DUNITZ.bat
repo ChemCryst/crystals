5240ca4da00375fb260d0619a12453cbd56027f2
Richard Cooper <richardiancooper@gmail.com>
2018-10-29 15:45:57 +0000

    Update build/setupenv.DUNITZ.bat
    Don't set COMPCODE in here, it gets modified to specify different test outputs.

--- build/setupenv.DUNITZ.bat
+++ build/setupenv.DUNITZ.bat
@@ -1,9 +1,6 @@
 
 call "E:\Program Files\IntelSWTools\compilers_and_libraries_2017.4.210\windows\bin\ipsxe-comp-vars.bat" intel64 vs2015
 
-SET COMPCODE=INW
-SET CROPENMP=TRUE
-set CR64BIT=TRUE
 set WXNUM=31
 set WXVC=vc140_x64
 set WXMINOR=0
