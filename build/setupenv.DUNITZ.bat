b5d3aac96b7e2b9cb4e22c45b197c2f0278f5e50
Richard Cooper <richardiancooper@gmail.com>
2018-10-29 12:06:32 +0000

    Update build/setupenv.DUNITZ.bat
    Do 64 bit build on Windows

--- build/setupenv.DUNITZ.bat
+++ build/setupenv.DUNITZ.bat
@@ -1,8 +1,9 @@
 
-call "E:\Program Files\IntelSWTools\compilers_and_libraries_2017.4.210\windows\bin\ipsxe-comp-vars.bat" ia32 vs2015
+call "E:\Program Files\IntelSWTools\compilers_and_libraries_2017.4.210\windows\bin\ipsxe-comp-vars.bat" ia64 vs2015
 
 SET COMPCODE=INW
 SET CROPENMP=TRUE
+set CR64BIT=TRUE
 set WXNUM=31
 set WXVC=vc140
 set WXMINOR=0
