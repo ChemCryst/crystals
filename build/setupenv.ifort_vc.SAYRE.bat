f217136c3c52c179189017c6be2ae4db8fd7d9d1
richardicooper <richardiancooper@gmail.com>
2019-05-13 16:49:32 +0100

    First steps - setup file and Jenkins section

---
+++ build/setupenv.ifort_vc.SAYRE.bat
@@ -0,0 +1,12 @@
+call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.1.156\windows\bin\ipsxe-comp-vars.bat" intel64 vs2015
+set WXNUM=31
+set WXVC=vc140_x64
+set WXMINOR=0
+SET WXWIN=c:\wx
+SET WXLIB=%WXWIN%\lib\%WXVC%_dll
+set PATH=%WXLIB%;%PATH%
+set INNOSETUP=c:\program files (x86)\Inno Setup 5
+
+SETLOCAL ENABLEEXTENSIONS
+setlocal EnableExtensions EnableDelayedExpansion
+
