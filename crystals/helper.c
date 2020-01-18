01ed7913b7df523ac32c2a22650e016b4360bff7
Richard Cooper <richardiancooper@gmail.com>
2020-01-18 17:08:31 +0000

    build update
    direct.h only in non-intel win32 build
    create empty startup scripts for testing

--- crystals/helper.c
+++ crystals/helper.c
@@ -1,10 +1,10 @@
 // Helper functions that do things that Fortran can't do so portably.
 
-#include <direct.h>
 
 #ifdef CRY_FORTINTEL
 // Not this one.
 #elif CRY_OSWIN32
+   #include <direct.h>
 // but all the other Win32 compilers.
 // Make a directory - Windows version, non Intel compilers (e.g. gcc/gfortran).
     void cry_mkdir(const char *dir) {
