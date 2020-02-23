5b634c4faee92a3d4931d357c1050134db7d6ca4
Richard Cooper <richardiancooper@gmail.com>
2020-02-23 16:41:36 +0000

    Return non-zero exit if any diffs fail

--- bits/shelx2cry/regressions_tests/run.sh
+++ bits/shelx2cry/regressions_tests/run.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+difffail = 0
 
 for path in insfiles/*.ins; do
     [ -e "$path" ] || continue
@@ -10,7 +12,20 @@ for path in insfiles/*.ins; do
     ../shelx2cry -o "${file}.out" -l "${file}.log" $path > "${file}.stdout"
 
     diff  --strip-trailing-cr  "${file}.out" "references/${file}.out"
+    if [ $? -ne 0 ]
+    then
+      difffail = 1
+    fi
     diff  --strip-trailing-cr  "${file}.log" "references/${file}.log"
+    if [ $? -ne 0 ]
+    then
+      difffail = 1
+    fi
     diff  --strip-trailing-cr  "${file}.stdout" "references/${file}.stdout"
-
+    if [ $? -ne 0 ]
+    then
+      difffail = 1
+    fi
 done
+
+exit $difffail
