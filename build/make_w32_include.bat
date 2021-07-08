71ab0700818a084ade9ea5b23540b66ce94bbb53
Richard Cooper <richardiancooper@gmail.com>
2021-07-08 10:17:33 +0100

    Get version syntax correct for old Intel build

--- build/make_w32_include.bat
+++ build/make_w32_include.bat
@@ -18,7 +18,7 @@
 @
 @if "%CRYSVNVER%" == "Unversioned directory" ( @for /f "delims=" %%a in ('git rev-list HEAD --count') do @set CRYSVNVER=%%a)
 @if "%CRYSVNVER%" == "00000" ( @for /f "delims=" %%a in ('git rev-list HEAD --count') do @set CRYSVNVER=%%a)
-@set CRYVERSUM=10.1.100
+@set CRYVERSUM=1001100
 @
 @echo CrySVNver = %CRYSVNVER%
 :SVER
