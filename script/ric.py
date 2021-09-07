8c05682d36550049efe433bbbf93c6d2ea426d76
Richard Cooper <richardiancooper@gmail.com>
2021-09-07 21:23:03 +0100

    Update ric.py

--- script/ric.py
+++ script/ric.py
@@ -1,8 +1,9 @@
-import crys_run
+import crystals
 
 print("Hello Richard")
 
-print( dir(crys_run) )
+print( dir(crystals) )
 
+crystals.run(["#sum l 5", "end"])
 
-crys_run.crys_run("Hi........")
+#crys_run.crys_run("Hi........")
