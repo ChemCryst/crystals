7fe95279ecc1ef6251c09162861537170d0447d0
Richard Cooper <richardiancooper@gmail.com>
2021-10-01 10:33:03 +0100

    Include example code in the module file for future reference

--- crystals/ffhash_inc.f90
+++ crystals/ffhash_inc.f90
@@ -7,6 +7,404 @@
   ! #include "ffhash_inc.f90"
   ! end module m_ffhash
 
+  ! EXAMPLE CODE:
+  
+  
+  
+! ================= SIMPLE EXAMPLE ===============
+!! compile like this:
+!! ifort hashexample.f90 /fpp
+!
+!module m_example
+!  use iso_fortran_env
+!  implicit none
+!
+!  type, public :: my_type
+!     integer :: n
+!  end type my_type
+!
+!#define FFH_KEY_TYPE type(my_type)
+!#define FFH_CUSTOM_KEYS_EQUAL
+!#define FFH_VAL_TYPE integer(int64)
+!#include "ffhash_inc.f90"
+!
+!  pure logical function keys_equal(a, b)
+!    type(my_type), intent(in) :: a, b
+!    keys_equal = (a%n == b%n)
+!  end function keys_equal
+!end module m_example
+!
+!program test
+!  use m_example
+!  use iso_fortran_env
+!
+!  implicit none
+!
+!  type(ffh_t) :: h
+!  type(my_type) :: x, y
+!
+!  x%n = 123
+!  y%n = 345
+!
+!  call h%ustore_value(x, 1024_int64)
+!  call h%ustore_value(y, 2048_int64)
+!
+!  if (h%fget_value(x) == 1024_int64) then
+!     print *, "PASSED"
+!  else
+!     error stop "FAILED"
+!  end if
+!
+!  call h%reset()
+!end program test
+!
+! ================= HKL A,B EXAMPLE =========================  
+!! compile like this:
+!! ifort fabexample.f90 /fpp
+!
+!module m_hkl_ab_table
+!  use iso_fortran_env
+!  implicit none
+!
+!  type, public :: hkl_type
+!     integer :: h
+!     integer :: k
+!     integer :: l
+!  end type hkl_type
+!
+!  type, public :: ab_type
+!     real :: a
+!     real :: b
+!	 contains
+!	   procedure ab_unequal
+!	   generic :: operator(/=) => ab_unequal
+!	   procedure ab_equal
+!	   generic :: operator(==) => ab_equal
+!  end type ab_type
+!
+!#define FFH_KEY_TYPE type(hkl_type)
+!#define FFH_CUSTOM_KEYS_EQUAL
+!#define FFH_VAL_TYPE type(ab_type)
+!#include "ffhash_inc.f90"
+!
+!  logical function ab_unequal(lhs,rhs)
+!     class(ab_type), intent(in) :: lhs, rhs
+!	 ab_unequal = ( lhs%a /= rhs%a ) .or. ( lhs%b /= rhs%b )
+!  end function ab_unequal
+!  logical function ab_equal(lhs,rhs)
+!     class(ab_type), intent(in) :: lhs, rhs
+!	 ab_equal = .not. ab_unequal(lhs,rhs)
+!  end function ab_equal
+!
+!  pure logical function keys_equal(a, b)
+!    type(hkl_type), intent(in) :: a, b
+!    keys_equal = (a%h == b%h) .and. ( a%k == b%k ) .and. ( a%l == b%l )
+!  end function keys_equal
+!
+!end module m_hkl_ab_table
+!
+!
+!module ab_dataset
+!  use iso_fortran_env, dp => real64
+!  use m_hkl_ab_table
+!  implicit none
+!  type, public :: ab_data
+!	  type(ffh_t) :: hdict
+!	  logical :: sym_init = .false.
+!	  integer :: sym_nmatrices = 0
+!	  real(dp) :: sym_matrix(12,48)
+!	  
+!	  contains
+!	      procedure, pass :: addsym => data_addsym
+!	      procedure, pass :: sethkl => data_sethkl
+!	      procedure, pass :: gethkl => data_gethkl
+!  end type		  
+!  
+!  contains
+!  
+!	  subroutine data_addsym(self, matrix)
+!		class(ab_data), intent(inout) :: self
+!		real(dp) :: matrix(12)
+!		self%sym_nmatrices = self%sym_nmatrices + 1
+!		self%sym_matrix(:,self%sym_nmatrices) = matrix
+!		self%sym_init = .true.
+!	  end subroutine
+!
+!	  integer function data_gethkl(self, h,k,l, a,b)
+!		class(ab_data), intent(inout) :: self
+!	    integer, intent(in) :: h, k, l
+!		real, intent(out) :: a, b
+!		type(ab_type) :: ab
+!		type(hkl_type) :: hkl 
+!
+!		data_gethkl = 0
+!		a = 0.0
+!		b = 0.0
+!		hkl = hkl_type(h,k,l)
+!		call self%hdict%get_value(hkl, ab, data_gethkl)
+!      end function
+!
+!	  integer function data_sethkl(self, h,k,l,a,b)
+!		class(ab_data), intent(inout) :: self
+!	    integer, intent(in) :: h, k, l
+!		real, intent(in) :: a, b
+!		integer :: i, hp, kp, lp
+!		type(ab_type) :: ab
+!		type(hkl_type) :: hkl 
+!		
+!		if (.not. self%sym_init) then
+!		   data_sethkl =  -1
+!		else
+!			ab = ab_type(a,b)
+!		    do i = 1, self%sym_nmatrices
+!				hp = self%sym_matrix(1,i) * h +    &
+!				     self%sym_matrix(4,i) * k +    &
+!				     self%sym_matrix(7,i) * l +    &
+!					 self%sym_matrix(10,i) 
+!				kp = self%sym_matrix(2,i) * h +    &
+!				     self%sym_matrix(5,i) * k +    &
+!				     self%sym_matrix(8,i) * l +    &
+!					 self%sym_matrix(11,i) 
+!				lp = self%sym_matrix(3,i) * h +    &
+!				     self%sym_matrix(6,i) * k +    &
+!				     self%sym_matrix(9,i) * l +    &
+!					 self%sym_matrix(12,i) 
+!
+!				hkl = hkl_type(hp,kp,lp)
+!				print *, h,k,l,hp,kp,lp
+!				
+!		        call self%hdict%ustore_value(hkl, ab)
+!		    end do
+!		
+!		   data_sethkl = 0
+!		endif
+!	  end function
+!
+!end module
+!
+!
+!!      M2=L2
+!!      M2T=L2T
+!!      DO LJZ=1,N2
+!!c compute h' = S.h
+!!        STORE(M2T)=STORE(M6)*STORE(M2)+STORE(M6+1)*STORE(M2+3)
+!!     2   +STORE(M6+2)*STORE(M2+6)
+!!        STORE(M2T+1)=STORE(M6)*STORE(M2+1)+STORE(M6+1)*STORE(M2+4)
+!!     2   +STORE(M6+2)*STORE(M2+7)
+!!        STORE(M2T+2)=STORE(M6)*STORE(M2+2)+STORE(M6+1)*STORE(M2+5)
+!!     2   +STORE(M6+2)*STORE(M2+8)
+!!c calculate the h.t terms
+!!        STORE(M2T+3)=(STORE(M6)*STORE(M2+9)+STORE(M6+1)*STORE(M2+10)
+!!     2   +STORE(M6+2)*STORE(M2+11))
+!
+!
+!
+!subroutine fabstuff()
+!  use ab_dataset
+!  use iso_fortran_env, dp => real64
+!
+!  type(ab_data) :: fullhkldict
+!  real(dp) :: matrix1(12), matrix2(12) ! 
+!  real :: a,b
+!  
+!  
+!  matrix1 = (/ 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 /)
+!  matrix2 = (/ -1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0 /)
+!  
+!! Use ab_dataset to hide details and add symmetry generating initialisation
+!  
+!  i = fullhkldict%sethkl(1,1,1,0.5,0.4)   ! should return -1 (error) as sym is not initialized
+!
+!  if ( i == -1 ) then
+!     print *, "PASSED uninit check"
+!  else
+!     error stop "FAILED"
+!  end if
+!  
+!  call fullhkldict%addsym(matrix1)
+!  call fullhkldict%addsym(matrix2)
+!
+!  i = fullhkldict%sethkl(1,1,1,0.5,0.4)   ! should return 0 (OK) as sym is initialized
+!  i = fullhkldict%sethkl(1,0,0,0.6,0.5)   ! should return 0 (OK) as sym is initialized
+!  i = fullhkldict%sethkl(0,1,0,0.7,0.6)   ! should return 0 (OK) as sym is initialized
+!  if ( i == 0 ) then
+!     print *, "PASSED init check"
+!  else
+!     error stop "FAILED"
+!  end if
+!
+!  i = fullhkldict%gethkl(1,1,1,a,b)
+!  print *, i,  1, 1, 1,a,b
+!  i = fullhkldict%gethkl(-1,-1,-1,a,b)
+!  print *, i, -1,-1,-1,a,b
+!  i = fullhkldict%gethkl(1,1,1,a,b)
+!  print *, i,  1, 1, 1,a,b
+!  i = fullhkldict%gethkl(-1,-1,-1,a,b)
+!  print *, i, -1,-1,-1,a,b
+!  i = fullhkldict%gethkl(2,2,2,a,b)
+!  print *, i,  2, 2, 2,a,b
+!  i = fullhkldict%gethkl(-2,-2,-2,a,b)
+!  print *, i, -2,-2,-2,a,b
+!  i = fullhkldict%gethkl(1,0,0,a,b)
+!  print *, i, 1,0,0,a,b
+!  i = fullhkldict%gethkl(-1,0,0,a,b)
+!  print *, i, -1,0,0,a,b
+!  i = fullhkldict%gethkl(0,1,0,a,b)
+!  print *, i, 0, 1,0,a,b
+!  i = fullhkldict%gethkl(0,-1,0,a,b)
+!  print *, i, 0, -1,0,a,b
+!  i = fullhkldict%gethkl(0,0,1,a,b)
+!  print *, i, 0,0,1,a,b
+!  i = fullhkldict%gethkl(0,0,-1,a,b)
+!  print *, i, 0,0,-1,a,b
+!
+!
+!end subroutine
+!
+!
+!program test
+!  use iso_fortran_env, dp => real64
+!  use m_hkl_ab_table
+!  implicit none
+!
+!  type(ffh_t) :: h
+!  type(hkl_type) :: x, y, z
+!  type(ab_type) :: v1, v2, v3, vab
+!  integer, parameter    :: hkl_max = 85    ! should generate about five million combinations
+!  integer               :: nh, nk, nl, i, status
+!  integer, allocatable  :: keys(:)
+!  integer, allocatable  :: key_counts(:)
+!  real(dp)              :: t_start, t_end
+!  real :: abrand(2)
+!
+!  x%h = 123
+!  x%k = 234
+!  x%l = 345
+!  y%h = 56
+!  y%k = 67
+!  y%l = 78
+!  z = hkl_type(123,234,345)   ! positional member variable initialisation
+!  
+!  v1 = ab_type(1.00, -0.5)
+!  v2 = ab_type(3.14, 2.72)
+!  v3 = ab_type(-0.717, -1.54)
+!  
+!  call h%ustore_value(x, v1)
+!  call h%ustore_value(y, v2)
+!
+!  print *, h%fget_value(x)
+!  print *, h%fget_value(y)
+!  print *, h%fget_value(z)
+!  if (h%fget_value(z) .eq. ab_type(1.0,-0.5)) then
+!     print *, "PASSED equality"
+!  else
+!     error stop "FAILED"
+!  end if
+!
+!  if (h%fget_value(z) .ne. ab_type(2.0,-0.5)) then
+!     print *, "PASSED inequality 1"
+!  else
+!     error stop "FAILED"
+!  end if
+!
+!  if (h%fget_value(y) .ne. ab_type(3.14,2)) then
+!     print *, "PASSED inequality 2"
+!  else
+!     error stop "FAILED"
+!  end if
+!  
+!! benchmark five million
+!
+!  call cpu_time(t_start)
+!  do nh = -hkl_max, hkl_max
+!    do nk = -hkl_max, hkl_max
+!      do nl = -hkl_max, hkl_max
+!	    call random_number(abrand)
+!	    call h%ustore_value(hkl_type(nh,nk,nl), ab_type(abrand(1), abrand(2)))
+!	  end do
+!	end do
+!  end do
+!  call cpu_time(t_end)
+!
+!  write(*,*) "Storage benchmark"
+!  write(*, "(A,E12.4)") "Elapsed time (s) ", t_end - t_start
+!  write(*, "(A,I12)")   "n_keys_stored    ", h%n_keys_stored
+!  write(*, "(A,I12)")   "n_occupied       ", h%n_occupied
+!  write(*, "(A,I12)")   "n_buckets        ", h%n_buckets
+!
+!! replace all values
+!
+!  call cpu_time(t_start)
+!  do nh = -hkl_max, hkl_max
+!    do nk = -hkl_max, hkl_max
+!      do nl = -hkl_max, hkl_max
+!	    call h%ustore_value(hkl_type(nh,nk,nl), ab_type(nh*1.0, nk*100.0 + nl * 1.0))
+!	  end do
+!	end do
+!  end do
+!  call cpu_time(t_end)
+!
+!  write(*,*) "Storage benchmark 2"
+!  write(*, "(A,E12.4)") "Elapsed time (s) ", t_end - t_start
+!  write(*, "(A,I12)")   "n_keys_stored    ", h%n_keys_stored
+!  write(*, "(A,I12)")   "n_occupied       ", h%n_occupied
+!  write(*, "(A,I12)")   "n_buckets        ", h%n_buckets
+!
+!! retreive and check all values
+!
+!  call cpu_time(t_start)
+!  
+!  do nh = -hkl_max, hkl_max
+!    do nk = -hkl_max, hkl_max
+!      do nl = -hkl_max, hkl_max
+!	    vab = h%fget_value(hkl_type(nh,nk,nl))
+!		if ( vab /= ab_type(nh*1.0, nk*100.0 + nl * 1.0)) then
+!			print *, "Val check error: ", nh,nk,nl, vab
+!		end if
+!	  end do
+!	end do
+!  end do
+!  call cpu_time(t_end)
+!
+!  
+!  write(*,*) "Storage benchmark 3"
+!  write(*, "(A,E12.4)") "Elapsed time (s) ", t_end - t_start
+!  write(*, "(A,I12)")   "n_keys_stored    ", h%n_keys_stored
+!  write(*, "(A,I12)")   "n_occupied       ", h%n_occupied
+!  write(*, "(A,I12)")   "n_buckets        ", h%n_buckets
+!
+!! check some values
+!
+!  print *, h%fget_value(hkl_type(1,1,1))
+!  print *, h%fget_value(hkl_type(1,2,3))
+!  print *, h%fget_value(hkl_type(84,84,84))
+!
+!
+!  ! Count number of keys that occur an odd number of times
+!!  allocate(key_counts(minval(keys):maxval(keys)))
+!!  key_counts = 0
+!
+!!        key_counts(keys(n)) = key_counts(keys(n)) + 1
+!!  end do
+!!  n = sum(iand(key_counts, 1))
+!
+!!  if (n /= h%n_keys_stored) then
+!!     error stop "FAILED"
+!!  else
+!!     print *, "PASSED"
+!!  end if
+!
+!  ! Clean up allocated storage
+!  call h%reset()
+!!  deallocate(key_counts)
+!
+!  call fabstuff
+!	
+!
+!end program test
+!  
+!
+
   ! Special handling of strings (which can be shortened)
 #ifdef FFH_KEY_IS_STRING
 #define FFH_KEY_ARG character(len=*)
