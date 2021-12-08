c2ea283bd7135b8feffb7752ee726f03496ef7a1
Richard Cooper <richardiancooper@gmail.com>
2021-12-08 14:10:07 +0000

    Discamb changes
    Update discamb.py to use residues and charges to setup discamb jobs.
    Update xparam to allow atomic charges to be stored in L5.
    Added discamb calc to refine menu.

--- script/discamb.py
+++ script/discamb.py
@@ -41,6 +41,7 @@ disc2tsc_info = {    'basis set':   argdict['basis'],
                      'n cores':     argdict['ncores'],
                      'memory':      argdict['memory'],
                      'qm program':  'Orca',
+                     'qm structure': 'qm_sys',
                      'multipoles': { 'l max': 2, 'threshold': 15.0 }
                 }
 
@@ -106,7 +107,200 @@ def _find_discamb():
             break
 
     return _path 
+    
+    
+atomic_numbers = {
+    'H' : 1,
+    'HE':  2,
+    'LI':  3,
+    'BE': 4,
+    'B' : 5,
+    'C' : 6,
+    'N' : 7,
+    'O' : 8,
+    'F' : 9,
+    'NE': 10,
+    'NA': 11,
+    'MG': 12,
+    'AL': 13,
+    'SI': 14,
+    'P' : 15,
+    'S' : 16,
+    'CL': 17,
+    'AR': 18,
+    'K' : 19,
+    'CA': 20,
+    'SC': 21,
+    'TI': 22,
+    'V' : 23,
+    'CR': 24,
+    'MN': 25,
+    'FE': 26,
+    'CO': 27,
+    'NI': 28,
+    'CU': 29,
+    'ZN': 30,
+    'GA': 31,
+    'GE': 32,
+    'AS': 33,
+    'SE': 34,
+    'BR': 35,
+    'KR': 36,
+    'RB': 37,
+    'SR': 38,
+    'Y' : 39,
+    'ZR': 40,
+    'NB': 41,
+    'MO': 42,
+    'TC': 43,
+    'RU': 44,
+    'RH': 45,
+    'PD': 46,
+    'AG': 47,
+    'CD': 48,
+    'IN': 49,
+    'SN': 50,
+    'SB': 51,
+    'TE': 52,
+    'I' : 53,
+    'XE': 54,
+    'CS': 55,
+    'BA': 56,
+    'LA': 57,
+    'CE': 58,
+    'PR': 59,
+    'ND': 60,
+    'PM': 61,
+    'SM': 62,
+    'EU': 63,
+    'GD': 64,
+    'TB': 65,
+    'DY': 66,
+    'HO': 67,
+    'ER': 68,
+    'TM': 69,
+    'YB': 70,
+    'LU': 71,
+    'HF': 72,
+    'TA': 73,
+    'W' : 74,
+    'RE': 75,
+    'OS': 76,
+    'IR': 77,
+    'PT': 78,
+    'AU': 79,
+    'HG': 80,
+    'TL': 81,
+    'PB': 82,
+    'BI': 83,
+    'PO': 84,
+    'AT': 85,
+    'RN': 86,
+    'FR': 87,
+    'RA': 88,
+    'AC': 89,
+    'TH': 90,
+    'PA': 91,
+    'U' : 92,
+    'NP': 93,
+    'PU': 94,
+    'AM': 95,
+    'CM': 96,
+    'BK': 97,
+    'CF': 98,
+    'ES': 99,
+    'FM': 100,
+    'MD': 101,
+    'NO': 102,
+    'LR': 103,
+    'RF': 104,
+    'DB': 105,
+    'SG': 106,
+    'BH': 107,
+    'HS': 108,
+    'MT': 109,
+    'DS': 110, 
+    'RG': 111, 
+    'CN': 112, 
+    'NH': 113,
+    'FL': 114,
+    'MC': 115,
+    'LV': 116,
+    'TS': 117,
+    'OG': 118
+}
 
+def get_crystals_atoms():
+
+    import re
+
+# Output data from CRYSTALS    
+    crystals.run(["#RELEASE PUNCH tmp/crystals.l5", "#PUNCH 5", "END"])
+    crystals.run(["#RELEASE PUNCH logs/bfile.pch"] )
+
+# Read it into a list of dicts
+    atomlist = []
+    
+    with open('tmp/crystals.l5') as file:  
+        
+        atom = {}
+        aline = 0
+
+        for line in file: 
+        
+#            print (line)
+        
+            if len(line) == 0: continue
+            if line[0] == '#': continue
+            if line[0] == ' ': continue
+            if line[0] == '&': continue
+            if line[0] == '\\': continue
+            strs = re.split(r"[\s,=]\s*", line.strip())
+            if len(strs) == 0: continue
+
+#            print( len(strs), strs )
+
+            if aline == 0:
+                if strs[0] == 'ATOM':
+                    #print (strs[0] + strs[1])
+                    atom['type'] = strs[1]
+                    atom['serial'] = int(round(float(strs[2])))
+                    atom['occ'] = strs[3]
+                    atom['flag'] = strs[4]
+                    atom['x'] = strs[5]
+                    atom['y'] = strs[6]
+                    atom['z'] = strs[7]
+                    aline = 1
+                    continue
+
+            elif aline == 1:
+                if strs[0] == 'CON' and strs[1] == 'U[11]' and len(strs) == 8:
+                    atom['U11'] = strs[2]
+                    atom['U22'] = strs[3]
+                    atom['U33'] = strs[4]
+                    atom['U23'] = strs[5]
+                    atom['U13'] = strs[6]
+                    atom['U12'] = strs[7]
+                    aline = 2
+                    continue
+                else:
+                    raise Exception("Error 002 reading crystals.l5")
+                
+            elif aline == 2:
+                if strs[0] == 'CON' and strs[1] == 'SPARE' and len(strs) ==7:
+                    atom['spare'] = strs[2]
+                    atom['part'] = strs[3]
+                    atom['ref'] = strs[4]
+                    atom['residue'] = int(strs[5])
+                    atom['new'] = int(strs[6])
+                    atomlist.append(atom)
+                    atom = {}
+                    aline = 0
+                    continue
+                else:
+                    raise Exception("Error 003 reading crystals.l5")
+            
+    return atomlist
 
 
 
@@ -153,15 +347,60 @@ try:
     import crystals
     crystals.run(["#FORE SHELX HARD", "END"])
     crystals.run(["#RELE PUNCH crystals.hkl", 
-                  "#PUNCH 6 F", 
-                  "END", 
-                  "#RELEASE PUNCH logs/bfile.pch"] )
+                  "#PUNCH 6 F", "END"] )
+    crystals.run(["#RELEASE PUNCH logs/bfile.pch"] )
 except Exception as e:
     print(sys.exc_info())
     raise Exception("discamb.py must be run in CRYSTALS")
 
 copy('crystals.hkl', work_folder )
 copy('shelxs.ins', work_folder + os.sep + 'crystals.ins' )
+
+atoms = get_crystals_atoms()
+
+#for atom in atoms:
+#    print(f"{atom['type']} {atom['serial']} {atom['x']} {atom['y']} {atom['z']} {atom['residue']}")
+   
+residue_vals = set( [ r['residue'] for r in atoms ] )   
+
+print (f" -- There are {len(residue_vals)} residue(s): ")
+
+all_residues = dict()
+
+net_charge = 0
+
+for res in residue_vals:
+#    print (res)
+    residue = dict()  # store properties for each residue in here.
+    all_residues[res] = residue # and store each of these residues in here.
+
+    #sum charge ('new') for each residue
+    charge = sum( [ r['new'] for r in atoms if r['residue'] == res ] )
+    net_charge += charge
+    residue['charge'] = charge
+    print(f"\n -- Charge on res {res} is {charge}")
+    
+    #sum n_electrons for each residue
+    nelec = sum( [ atomic_numbers[r['type'].upper()] for r in atoms if r['residue'] == res ] )
+    residue['nelec'] = nelec - charge
+    residue['mult'] = (nelec - charge)%2 + 1
+    print(f" -- N electrons on res {res} is {nelec - charge}, spin mult is {residue['mult']}")
+    
+    #get spin mult
+if ( net_charge != 0 ):
+    print ("{E Warning net charge on all molecules is not zero")
+
+print (f"Net charge on all molecules is {net_charge}")
+
+# Make a set of separate molecules using the 'residue' number.
+
+with open(work_folder + os.sep + 'qm_sys', 'w') as file:  
+    for resid,res in all_residues.items():
+        file.write(f"System r{resid} {res['charge']} {res['mult']}\n")
+        atom = [ r for r in atoms if r['residue'] == resid ][0]
+        file.write(f"$connect {atom['type']}{atom['serial']}\n")
+
+
 
 def deletefiles ( filelist ):
 
@@ -189,6 +428,7 @@ print ("Launching discamb2tsc. This may take several minutes to hours.")
 #              )
 
 
+#Comment out for testing
 subprocess.run( [ discambexe, "crystals.ins", "crystals.hkl" ], 
                 stdout = subprocess.PIPE, stderr = subprocess.STDOUT,
                 cwd = work_folder, shell=True,
