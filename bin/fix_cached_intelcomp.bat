633daec1d03493037bf472f370db081f32f2f7a3
Richard Cooper <richardiancooper@gmail.com>
2022-02-17 20:17:27 +0000

    Try restoring symbolic links after cache restore (oneAPI)

---
+++ bin/fix_cached_intelcomp.bat
@@ -0,0 +1,20 @@
+for /f "delims=" %%d in ( 'dir /a:d /b "C:\Program Files (x86)\Intel\oneAPI"' ) do call :DoIt "%%d"
+goto EndDoIt
+
+:DoIt
+  set "componentpath=C:\Program Files (x86)\Intel\oneAPI\%~1"
+  set "cname=%~n1"
+
+  echo: Componentpath: '%componentpath%'
+  echo: cname: '%cname%'
+  
+  rem delete existing symbolic link
+  
+  del "%componentpath%\latest"
+  
+  for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+
+  mklink /d "%componentpath%\latest" "%componentpath%\%LATEST_VERSION%"
+
+exit /b
+:EndDoIt
