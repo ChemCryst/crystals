43945bf9fb6ecca2596dde8678c59091814b579a
Richard Cooper <richardiancooper@gmail.com>
2022-02-17 20:53:26 +0000

    More logging

--- bin/fix_cached_intelcomp.bat
+++ bin/fix_cached_intelcomp.bat
@@ -10,9 +10,12 @@ goto EndDoIt
   
   rem delete existing symbolic link
   
+  dir %componentpath%
+  
   if EXIST "%componentpath%\latest" (
       rmdir "%componentpath%\latest"
       for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+      echo: make link to: "%componentpath%\%LATEST_VERSION%"
       mklink /d "%componentpath%\latest" "%componentpath%\%LATEST_VERSION%"
   )
 
